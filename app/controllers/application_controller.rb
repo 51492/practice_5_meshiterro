323e369426e76bb78470479b4ec3a505c022ccde
TeppeiYamashita <143589587+51492@users.noreply.github.com>
2023-09-14 14:08:24 +0000

    [update]meshiterro14

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   #上記でdevise利用の機能(ユーザ登録、ログイン認証など)が使われる前にconfigure_permitted_parametersメソッドが実行される
 
   def after_sign_in_path_for(resource) #after_sign_in_path_forはdeviseが用意しているメソッドで、サインイン後にどこに遷移するかを設定している。(初期値は「root_path」)引数resourceにはログインしたモデルのデータ(ログインしたUserのインスタンスが格納される)
-    about_path
+    post_images_path
   end
 
   def after_sign_out_path_for(resource) #上記のafter_sign同様、deviseが用意しているメソッド(初期値は「root_path」)
